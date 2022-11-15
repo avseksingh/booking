bcba57772f267957346356235ac8b7ff76976a9d
Abhishek Singh <avsekjob@gmail.com>
2022-11-15 13:05:23 +0530

    login / registrtion working

--- booking/bookingapp/models.py
+++ booking/bookingapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class login(models.Model):
+    mobile = models.IntegerField()
+    otp = models.IntegerField()
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return ("Mobile Number = {}, OTP = {}, Time = {}". format(self.mobile, self.otp, self.time))
--- booking/bookingapp/views.py
+++ booking/bookingapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, HttpResponse
+import random
 
 # Create your views here.
 
-def index(self):
-    return HttpResponse("This is Booking App")
+def index(request):
+    
+    return render(request, "index.html")
+
+def rand(request):
+    rnumber = random.randint(1000,9999)
+    return HttpResponse(rnumber)
